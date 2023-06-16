169e25a771ab415db1c565606538633a1928a749
unknown <joshua.pollard.stu@learn.sd23.bc.ca>
2023-06-15 17:51:21 -0700

    adding files for polyrun game

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("ChallengeObj Game Object")]
+    public GameObject challengeObject;
+    [Header("Default spawn delay time")]
+    public float spawnDelay = 1f;
+    [Header("Default spawn time")]
+    public float spawnTime = 2f;
+void Start()
+{
+    InvokeRepeating("InstantiateObjects", spawnDelay, spawnTime);
+}
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+transform.position = new Vector3(15, -2.75f, 0);
+    }
+    void InstantiateObjects()
+    {
+        Instantiate(challengeObject, transform.position, transform.rotation);
+
+    }
+}
